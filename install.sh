0c32b656d0e3fad424d7ff1a5a9fc1cb0c3445d7
timetravelthree <dantexor@protonmail.com>
2023-03-29 06:58:48 -0800

    fixed grammar

--- install.sh
+++ install.sh
@@ -102,15 +102,13 @@ EOH
 	set +x
 fi
 
-log "Installing Gef24 script" info
-set -x
+log "Installing Gef24 script into ${HOME}/.local/bin/gef24" info
 cat <<EOH >"${HOME}/.local/bin/gef24"
 #!/bin/sh
 #exec gdb -q -ex init-gef24 "$@"
 EOH
-set +x
 
 log "Installation finished successfully!" success
-log 'Now try running `gef24 ${BINARY_NAME}`' success
+log 'Now try to run `gef24 ${BINARY_NAME}`' success
 
 exit 0
